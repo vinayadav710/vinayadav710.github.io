3de58141ac32f4ecf6055690c35507176182065c
Vinay Kumar <113518480+vinayadav710@users.noreply.github.com>
2024-05-28 17:07:43 +0530

    tried simpli static

---
+++ wp-content/uploads/uag-plugin/assets/3000/uag-js-2974.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dde001fe' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9252c31c' );
+});
+ });
