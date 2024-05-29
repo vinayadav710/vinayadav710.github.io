b2ee1d3d35f1c2f25982b5a5b937c331016e2d5f
Vinay Kumar <113518480+vinayadav710@users.noreply.github.com>
2024-05-29 22:25:37 +0530

    Update internalPages

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-772.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-772.js
@@ -1,36 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-fc8358f0' );
-});
-var ssLinksParent = document.querySelector( '.uagb-block-69983425' );
-ssLinksParent?.addEventListener( 'keyup', function ( e ) {
-var link = e.target.closest( '.uagb-ss__link' );
-if ( link && e.keyCode === 13 ) {
-	handleSocialLinkClick( link );
-}
-});
-
-ssLinksParent?.addEventListener( 'click', function ( e ) {
-var link = e.target.closest( '.uagb-ss__link' );
-if ( link ) {
-	handleSocialLinkClick( link );
-}
-});
-
-function handleSocialLinkClick( link ) {
-var social_url = link.dataset.href;
-var target = "";
-if ( social_url == "mailto:?body=" ) {
-	target = "_self";
-}
-var request_url = "";
-if ( social_url.indexOf("/pin/create/link/?url=") !== -1 ) {
-	request_url = social_url + encodeURIComponent( window.location.href ) + "&media=" + '';
-} else {
-	request_url = social_url + encodeURIComponent( window.location.href );
-}
-window.open( request_url, target );
-}
-window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-944305bb' );
 });
 window.addEventListener( 'load', function() {
